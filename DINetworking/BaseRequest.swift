3c729f55a3eb2e76b44da4b9da8fe9110918e710
HoangNguyen <hoanguit@gmail.com>
2021-06-17 14:16:29 +0700

    DINetworking - init

---
+++ DINetworking/BaseRequest.swift
@@ -0,0 +1,51 @@
+//
+//  BaseRequest.swift
+//  DISample
+//
+//  Created by NguyenHoang on 6/17/21.
+//
+
+import Foundation
+import Moya
+
+protocol BaseRequest : TargetType {
+    var body:[String: Any] {get}
+}
+extension BaseRequest {
+    
+    public var baseURL: URL {
+        return URL(string: NetworkManager.instance.baseUrl)!
+    }
+    
+    public var method: Moya.Method {
+        switch self {
+        default: return .get
+        }
+    }
+    public var headers: [String: String]? {
+        return ["Content-Type": "application/json",
+                "AccessToken":NetworkManager.instance.token]
+    }
+    public var path: String {
+        return "/"
+    }
+    
+    
+    
+    public var body: [String : Any] {
+        get {
+            return [:]
+        }
+    }
+    
+    public var sampleData: Data {
+        return Data()
+    }
+    public var task: Task {
+        return .requestPlain
+    }
+    
+    public var validationType: ValidationType {
+        return .successCodes
+    }
+}
---
+++ DINetworking/DINetworking.h
@@ -0,0 +1,18 @@
+//
+//  DINetworking.h
+//  DINetworking
+//
+//  Created by NguyenHoang on 6/17/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DINetworking.
+FOUNDATION_EXPORT double DINetworkingVersionNumber;
+
+//! Project version string for DINetworking.
+FOUNDATION_EXPORT const unsigned char DINetworkingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DINetworking/PublicHeader.h>
+
+
