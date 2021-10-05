08f0d6e44f9f2e1e93f1de91aab2f3d0c30dacb2
trantuanvu91 <vu.tran@watasolutions.com>
2021-10-05 11:22:16 +0700

    update code hexString

--- CommonModule/Extensions/UIKit/UIColorExtensions.swift
+++ CommonModule/Extensions/UIKit/UIColorExtensions.swift
@@ -343,3 +343,57 @@ extension UIColor {
     static let backgroundImage: UIColor = UIColor(hex: 0xD8D8D8)
     static let disabledButton: UIColor = UIColor.black.withAlphaComponent(0.25)
 }
+
+extension UIColor {
+    convenience init(hex: Int, alpha: CGFloat = 1.0) {
+        self.init(
+            red: CGFloat((hex >> 16) & 0xFF) / 255.0,
+            green: CGFloat((hex >> 8) & 0xFF) / 255.0,
+            blue: CGFloat(hex & 0xFF) / 255.0,
+            alpha: alpha
+        )
+    }
+
+    // swiftlint:disable identifier_name
+    convenience init(hexString: String) {
+        let hex = hexString.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+        var int = UInt32()
+        Scanner(string: hex).scanHexInt32(&int)
+        let a, r, g, b: UInt32
+        switch hex.count {
+        case 3: // RGB (12-bit)
+            (a, r, g, b) = (255, (int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
+        case 6: // RGB (24-bit)
+            (a, r, g, b) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
+        case 8: // ARGB (32-bit)
+            (a, r, g, b) = (int >> 24, int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
+        default:
+            (a, r, g, b) = (255, 0, 0, 0)
+        }
+        self.init(red: CGFloat(r) / 255, green: CGFloat(g) / 255, blue: CGFloat(b) / 255, alpha: CGFloat(a) / 255)
+    }
+    // swiftlint:enable identifier_name
+
+    static let whiteSmoke = UIColor(hex: 0xf3f4f5, alpha: 1)
+    static let textColor = UIColor(hex: 0x556270, alpha: 1)
+
+    func lighter(by percentage: CGFloat = 15.0) -> UIColor? {
+        return self.adjust(by: abs(percentage) )
+    }
+
+    func darker(by percentage: CGFloat = 15.0) -> UIColor? {
+        return self.adjust(by: -1 * abs(percentage) )
+    }
+
+    func adjust(by percentage: CGFloat = 30.0) -> UIColor? {
+        var r: CGFloat=0, g: CGFloat = 0, b: CGFloat = 0, a: CGFloat = 0
+        if (self.getRed(&r, green: &g, blue: &b, alpha: &a)) {
+            return UIColor(red: min(r + percentage/100, 1.0),
+                           green: min(g + percentage/100, 1.0),
+                           blue: min(b + percentage/100, 1.0),
+                           alpha: a)
+        } else {
+            return nil
+        }
+    }
+}
