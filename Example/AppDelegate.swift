eb9fed424d33d84721338fb22c1f335896791b79
wata-tiendubai <83210150+wata-tiendubai@users.noreply.github.com>
2021-10-21 16:39:51 +0700

    Revert "setup config environment for baseUrl : staging, production, qa"

--- Example/AppDelegate.swift
+++ Example/AppDelegate.swift
@@ -14,11 +14,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        
-       // print(Configuration.current)
-        //print(Configuration.APIKey.apiKey)
-        
-        
         return true
     }
 
