7180ff2851a1cfd200cd99173eeac35c95d60841
Duy Nguyen <duyptit1986@gmail.com>
2021-11-09 12:20:40 +0700

    update nibname function

--- CommonModule/Extensions/UIKit/UIViewControllerExtensions.swift
+++ CommonModule/Extensions/UIKit/UIViewControllerExtensions.swift
@@ -154,6 +154,15 @@ public extension UIViewController {
             return 0
         }
     }
+    
+    //** loads instance from right framework bundle, not main bundle as UIViewController.init() does
+    private static func genericInstance<T: UIViewController>() -> T {
+        return T.init(nibName: String(describing: self), bundle: Bundle(for: self))
+    }
+
+    static func instance() -> Self {
+        return genericInstance()
+    }
 
 }
 
